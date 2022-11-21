c355bc32c6b30802b058411a9b9d80733a485488
mosmn <ithinkisee8@gmail.com>
2022-11-22 06:54:08 +0800

    created the add, subtract, multiply, divide, and power functions

---
+++ main.js
@@ -0,0 +1,38 @@
+/*
+PROJECT: create a simple calculator that can add, subtract, multiply, and divide
+
+BEGIN
+1) create a function that adds two numbers
+2) create a function that subtracts two numbers
+3) create a function that multiplies two numbers
+4) create a function that divides two numbers
+5) create a function that calculates the power of a number
+END
+*/ 
+
+//1) create a function that adds two numbers
+const add = (num1, num2) => {
+    return num1 + num2;
+    }
+
+//2) create a function that subtracts two numbers
+const subtract = (num1, num2) => {
+    return num1 - num2;
+    }
+
+//3) create a function that multiplies two numbers
+const multiply = (num1, num2) => {
+    return num1 * num2;
+    }
+
+//4) create a function that divides two numbers
+const divide = (num1, num2) => {
+    return num1 / num2;
+    }
+
+//5) create a function that calculates the power of a number
+const power = (num1, num2) => {
+    return num1 ** num2;
+    }
+
+    
