f7637e593d2f2f1a2d5ca911976b399340998fee
mosmn <ithinkisee8@gmail.com>
2022-11-22 07:11:58 +0800

    created Ans variable

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ BEGIN
 4) create a function that divides two numbers
 5) create a function that calculates the power of a number
 6) create a function that displays the result of the calculation
+7) make Ans a variable that stores the result of the previous calculation
 END
 
-git commit -m "created the add, subtract, multiply, divide, and power functions"
+git commit -m "created Ans variable"
 */ 
 
 //1) create a function that adds two numbers
@@ -41,9 +42,14 @@ const power = (num1, num2) => {
 //6) create a function that displays the result of the calculation
 const displayResult = (result) => {
     console.log(result);
+    return result;
     }
 
+//7) make Ans a variable that stores the result of the previous calculation
+let Ans = displayResult(add(2, 3));
+console.log(Ans);
 
+    
 
 
 
